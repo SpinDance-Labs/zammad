2fd3b734c8bf400d31fa950238f9fc0c33d4c75c
Chris Samuelson <chris@spindancelabs.com>
2021-03-15 04:09:30 -0400

    initial integration with Spree

--- lib/omniauth/oauth2_database.rb
+++ lib/omniauth/oauth2_database.rb
@@ -11,4 +11,39 @@ class Oauth2Database < OmniAuth::Strategies::OAuth2
     super
   end
 
+  uid { raw_info['data']['id'] }
+
+  info do
+    email = raw_info['data']['attributes']['email']
+    if raw_info['included'].count > 0
+      first_name = raw_info['included'][0]['attributes']['firstname']
+      last_name = raw_info['included'][0]['attributes']['lastname']
+      phone = raw_info['included'][0]['attributes']['phone']
+      name = "#{first_name} #{last_name}"
+    else
+      first_name = ""
+      last_name = ""
+      phone = ""
+      name = raw_info['data']['attributes']['email']
+    end
+    {
+      email: email,
+      first_name: first_name,
+      last_name: last_name,
+      phone: phone,
+      name: name
+    }
+  end
+
+  extra do
+    {
+      'raw_info' => raw_info
+    }
+  end
+
+  def raw_info
+    JSON.parse(access_token.get('/api/v2/storefront/account?include=default_billing_address').body)
+  end
+
+
 end
